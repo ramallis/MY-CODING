585726943261233ed4125aad6c2f90dc2d6a9d8d
Sophia Ramalli <144574912+ramallis@users.noreply.github.com>
2025-06-06 12:09:23 -0500

    Delete util.h

--- util.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
-SDL_Texture* render_image(SDL_Window* window, SDL_Renderer* renderer, const char *image_file);
